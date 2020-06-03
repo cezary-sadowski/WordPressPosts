5720bd69193abd73b753c4662426e5bc486df327
Cezary <cez94k@gmail.com>
2020-06-03 16:14:23 +0200

    get posts from api

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  private url: string = 'https://public-api.wordpress.com/rest/v1.1/sites/en.blog.wordpress.com/posts';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts() {
+    return this.http.get(this.url);
+  }
 }
